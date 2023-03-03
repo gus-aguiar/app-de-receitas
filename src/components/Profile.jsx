0bf163dc9f18cc1e05d57dfd319356ecf438974e
helma trindade <helmacaires28@gmail.com>
2023-03-03 17:49:51 -0300

    Requisito 60

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Profile() {
   const [email, setEmail] = useState('');
@@ -7,10 +8,20 @@ function Profile() {
     setEmail(JSON.parse(localStorage.getItem('user')).email);
   }, []);
 
+  const history = useHistory();
+  const handleClick = () => {
+    history.push('/done-recipes');
+  };
+
   return (
     <div>
       <h2 data-testid="profile-email">{email}</h2>
-      <button data-testid="profile-done-btn">Done Recipes</button>
+      <button
+        data-testid="profile-done-btn"
+        onClick={ handleClick }
+      >
+        Done Recipes
+      </button>
       <button data-testid="profile-favorite-btn">Favorite Recipes</button>
       <button data-testid="profile-logout-btn">Logout</button>
     </div>
