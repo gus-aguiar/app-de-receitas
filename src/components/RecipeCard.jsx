0e687aec1f0db3e5bff08d51ff82fbed1f2354b7
gus-aguiar <gustavopma90@gmail.com>
2023-03-03 14:27:59 -0300

    pagina de receitas, requisito 19, criei a página de receitas que renderiza drinks e meals e os botões de categoria

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,45 @@
+// Arquivo: src/components/RecipeCard.js
+
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function RecipeCard({ recipe, index }) {
+//   console.log(recipe);
+  const { strMeal, strDrink, strMealThumb, strDrinkThumb, idMeal, idDrink } = recipe;
+  const recipeType = strMeal ? 'meal' : 'drink';
+  const name = strMeal || strDrink;
+  const thumb = strMealThumb || strDrinkThumb;
+  const linkToRecipe = `/${recipeType}/${idMeal || idDrink}`;
+
+  return (
+    <Link
+      to={ linkToRecipe }
+      data-testid={ `${index}-recipe-card` }
+    >
+      <div
+        className="recipe-card"
+      >
+        <img
+          src={ thumb }
+          alt={ `Imagem de ${name}` }
+          data-testid={ `${index}-card-img` }
+        />
+        <p data-testid={ `${index}-card-name` }>{name}</p>
+      </div>
+    </Link>
+  );
+}
+
+RecipeCard.propTypes = {
+  index: PropTypes.any,
+  recipe: PropTypes.shape({
+    idDrink: PropTypes.string,
+    idMeal: PropTypes.string,
+    strDrink: PropTypes.string,
+    strDrinkThumb: PropTypes.string,
+    strMeal: PropTypes.string,
+    strMealThumb: PropTypes.string,
+  }),
+}.isrequired;
+
+export default RecipeCard;
