6396d8bf0ba7f9021a1e84ada5e4195340965139
eversondias <eversondias@pop-os.localdomain>
2023-03-03 15:19:36 -0300

    grupo 18

--- src/services/recipesAPI.js
+++ src/services/recipesAPI.js
@@ -19,3 +19,39 @@ export async function getDrinksCategories() {
   const data = await response.json();
   return data.drinks;
 }
+
+export async function getDrinksIngredients({ search }) {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${search}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getDrinksName({ search }) {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getDrinksFirstLetter({ search }) {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${search}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getMealsIngredients({ search }) {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getMealsName({ search }) {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getMealsFirstLetter({ search }) {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`);
+  const data = await response.json();
+  return data;
+}
