ed2d35eade6cd7cc1d241b265ff45d7976b31796
eversondias <eversondias@pop-os.localdomain>
2023-03-02 23:40:26 -0300

    requesito 13 fiz o redirecionamento para as paginas
    imprementei a logica que se a resposta da api for uma receita direcionar para a pagina de detales dela

--- src/context/myProvider.js
+++ src/context/myProvider.js
@@ -1,24 +1,46 @@
 import PropTypes from 'prop-types';
 import React, { useMemo, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import MyContext from './myContext';
 import { toggleAPI } from '../utils/getAPI';
 
 function Provider({ children }) {
-  const [ListOfProducts, setListOfProducts] = useState({});
+  const [listOfProducts, setListOfProducts] = useState({});
+  const history = useHistory();
 
   const handleSearch = async (event, page) => {
+    console.log('carregando');
     event.preventDefault();
     const { target: { form: { radio, search } } } = event;
     // faz a validação se deve mostra um alerta ou fazer um requisição a API
     if (radio.value === 'first-letter' && search.value.length > 1) {
       global.alert('Your search must have only 1 (one) character');
-    } else setListOfProducts(await toggleAPI(radio.value, search.value, page));
+    } else {
+      const response = await toggleAPI(radio.value, search.value, page);
+      setListOfProducts(response);
+      console.log(response);
+      if (response.meals) {
+        const { meals } = response;
+        if (meals.length === 1) {
+          const { idMeal } = meals[0];
+          history.push(`/meals/${idMeal}`);
+        }
+      }
+      if (response.drinks) {
+        const { drinks } = response;
+        console.log(drinks);
+        if (drinks.length === 1) {
+          const { idDrink } = drinks[0];
+          history.push(`/drinks/${idDrink}`);
+        }
+      }
+    }
   };
 
   const value = useMemo(() => ({
     handleSearch,
-    ListOfProducts,
-  }), [ListOfProducts]);
+    ListOfProducts: listOfProducts,
+  }), [listOfProducts]);
   return (
     <MyContext.Provider value={ value }>
       {children}
--- src/utils/getAPI.js
+++ src/utils/getAPI.js
@@ -1,4 +1,5 @@
 export async function toggleAPI(type, search, page) {
+  console.log('carregando');
   // type verifica qual input radio foi escolhido e faz a requisição baseado nisso.
   if (page === 'Drinks') {
     if (type === 'ingredient') {
