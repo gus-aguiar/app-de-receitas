7a650f030de06b8502b0e620ca091718ff471432
eversondias <eversondias@pop-os.localdomain>
2023-03-06 15:57:22 -0300

    [group 18] requesito 50 criar a pagina de receitas favoritas

--- src/components/FavoriteRecipes.jsx
+++ src/components/FavoriteRecipes.jsx
@@ -1,10 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function FavoriteRecipes() {
+  const [favoriteRecipes, setFavoriteRecipes] = useState(undefined);
+  useEffect(() => {
+    if (localStorage.getItem('favoriteRecipes')) {
+      const recipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      setFavoriteRecipes(recipes);
+    }
+  }, []);
   return (
     <div>
       <Header title="Favorite Recipes" />
+      <button data-testid="filter-by-all-btn">All</button>
+      <button data-testid="filter-by-meal-btn">Meals</button>
+      <button data-testid="filter-by-drink-btn">Drinks</button>
+      {favoriteRecipes
+      && favoriteRecipes.map((recipe, index) => (
+        recipe.type === 'meal' ? (
+          <div key={ recipe.id }>
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ recipe.image }
+              alt={ recipe.name }
+            />
+            <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+            <p
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              {`${recipe.nationality} - ${recipe.category}`}
+            </p>
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              src={ blackHeartIcon }
+              label={ recipe.name }
+            />
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ shareIcon }
+              label={ recipe.name }
+            />
+          </div>
+        )
+          : (
+            <div key={ recipe.id }>
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                src={ recipe.image }
+                alt={ recipe.name }
+              />
+              <p
+                data-testid={ `${index}-horizontal-name` }
+              >
+                {recipe.name}
+              </p>
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {recipe.alcoholicOrNot}
+              </p>
+              <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-favorite-btn` }
+                src={ blackHeartIcon }
+                label={ recipe.name }
+              />
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+                label={ recipe.name }
+              />
+            </div>)
+      ))}
     </div>
   );
 }
