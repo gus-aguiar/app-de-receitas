468769bb5a75ba636b8ad2a7100da2fd81de6ee2
gus-aguiar <gustavopma90@gmail.com>
2023-03-06 14:25:27 -0300

    requisitos 44, 45 e 46

--- src/components/DoneRecipes.jsx
+++ src/components/DoneRecipes.jsx
@@ -1,10 +1,88 @@
 import React from 'react';
 import Header from './Header';
+import shareIcon from '../images/shareIcon.svg';
 
 function DoneRecipes() {
+  // const recipes = [
+  //   {
+  //     id: '52874',
+  //     type: 'meal',
+  //     nationality: 'Italian',
+  //     category: 'beef',
+  //     alcoholicOrNot: 'non-alcoholic',
+  //     name: 'Beef and Mustard Pie',
+  //     image: 'https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg',
+  //     doneDate: '06/03/2023',
+  //     tags: ['pie'],
+  //   },
+  // ];
+
+  // localStorage.setItem('doneRecipes', JSON.stringify(recipes));
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  console.log(doneRecipes);
   return (
     <div>
       <Header title="Done Recipes" />
+      <button data-testid="filter-by-all-btn">All</button>
+      <button data-testid="filter-by-meal-btn">Meals</button>
+      <button data-testid="filter-by-drink-btn">Drinks</button>
+      {doneRecipes.map((recipe, index) => (
+        recipe.type === 'meal' ? (
+          <div key={ recipe.id }>
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ recipe.image }
+              alt={ recipe.name }
+            />
+            <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+            <p
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              {`${recipe.nationality} - ${recipe.category}`}
+            </p>
+
+            <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ shareIcon }
+              label="share"
+            />
+            {recipe.tags.map((tag, i) => (
+              <p data-testid={ `${index}-${tag}-horizontal-tag` } key={ i }>{tag}</p>
+            ))}
+          </div>
+        )
+          : (
+            <div key={ recipe.id }>
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                src={ recipe.image }
+                alt={ recipe.name }
+              />
+              <p
+                data-testid={ `${index}-horizontal-name` }
+              >
+                {recipe.name}
+              </p>
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {recipe.alcoholicOrNot}
+              </p>
+              <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+                label="share"
+              />
+
+            </div>)
+
+      ))}
+
     </div>
   );
 }
